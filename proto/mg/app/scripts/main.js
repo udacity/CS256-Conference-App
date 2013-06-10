da349994c90be08c6006e0827b8565a1d6560e86
Matt Gaunt <mattgaunt@google.com>
2013-06-10 16:22:06 +0100

    Some initial layout of the home screen

---
+++ proto/mg/app/scripts/main.js
@@ -0,0 +1,19 @@
+require.config({
+    paths: {
+        jquery: '../components/jquery/jquery',
+        bootstrap: 'vendor/bootstrap'
+    },
+    shim: {
+        bootstrap: {
+            deps: ['jquery'],
+            exports: 'jquery'
+        }
+    }
+});
+
+require(['app', 'jquery', 'bootstrap'], function (app, $) {
+    'use strict';
+    // use app here
+    console.log(app);
+    console.log('Running jQuery %s', $().jquery);
+});
